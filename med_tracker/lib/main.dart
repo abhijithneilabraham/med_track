973b080b604552ae922acd1360ca4f6598cce593
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2020-03-09 11:34:49 +0530

    Update med_tracker/lib/main.dart

--- med_tracker/lib/main.dart
+++ med_tracker/lib/main.dart
@@ -347,14 +347,42 @@ class ButtonClickEventState extends State<ButtonClickEvent> {
             )));
   }
 }
-class _MyHomePageJava extends StatefulWidget {
-  MyHomePageJavaState createState()={
-  return new _MyHomePageJavaState();
-  }
-
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
 }
-class _MyHomePageJavaState extends State<_MyHomePageJava>{
+class _MyHomePageState extends State<MyHomePage> {
   static const platform = const MethodChannel('flutter.native/helper');
+  String _responseFromNativeCode = 'Waiting for Response...';
+  Future<void> responseFromNativeCode() async {
+    String response = "";
+    try {
+      final String result = await platform.invokeMethod('helloFromNativeCode');
+      response = result;
+    } on PlatformException catch (e) {
+      response = "Failed to Invoke: '${e.message}'.";
+    }
+    setState(() {
+      _responseFromNativeCode = response;
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            RaisedButton(
+              child: Text('Call Native Method'),
+              onPressed: responseFromNativeCode,
+            ),
+            Text(_responseFromNativeCode),
+          ],
+        ),
+      ),
+    );
+  }
 }
-
-
