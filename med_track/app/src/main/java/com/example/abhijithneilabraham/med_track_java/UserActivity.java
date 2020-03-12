2d0e3e575445066d348169b59a3c298083b1d21d
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2020-03-12 20:22:14 +0530

    java code updation

--- med_track/app/src/main/java/com/example/abhijithneilabraham/med_track_java/UserActivity.java
+++ med_track/app/src/main/java/com/example/abhijithneilabraham/med_track_java/UserActivity.java
@@ -7,23 +7,35 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+
 public class UserActivity extends AppCompatActivity {
+    Button btnLogOut;
+    FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        btnLogOut = (Button) findViewById(R.id.btnLogOut);
+        btnLogOut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
+                FirebaseAuth.getInstance().signOut();
+                Intent I = new Intent(UserActivity.this, ActivityLogin.class);
+                startActivity(I);
+
             }
         });
-    }
 
+    }
 }
+
