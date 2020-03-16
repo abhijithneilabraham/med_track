a532a04a2a2356bcbc73559eae07e8cc902f2923
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2020-03-17 00:38:11 +0530

    updated spinner vals and db function

--- med_track/app/src/main/java/com/example/abhijithneilabraham/med_track_java/UserActivity.java
+++ med_track/app/src/main/java/com/example/abhijithneilabraham/med_track_java/UserActivity.java
@@ -18,25 +18,23 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-public class UserActivity extends AppCompatActivity {
+public class UserActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     Button btnLogOut;
     FirebaseAuth firebaseAuth;
     private FirebaseAuth.AuthStateListener authStateListener;
     String age;
     FirebaseDatabase database = FirebaseDatabase.getInstance();
     public String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
-
-Spinner spinner1;
+    Spinner spinner1;
     EditText details;
 
-    public void writeNewUser() {
-
-
-        database.getReference(uid).setValue("Hello");
-
-
+    public void writeNewUser(String head,String data) {
+        database.getReference(uid).child(head).setValue(data);
     }
     public void onItemSelected(AdapterView<?> parent, View view,
                                int pos, long id) {
@@ -54,21 +52,23 @@ Spinner spinner1;
         setContentView(R.layout.activity_user);
         spinner1=(Spinner)findViewById(R.id.spinner1);
         details=(EditText)findViewById(R.id.usr);
-        //String data = spinner1.getSelectedItem().toString();  // Intent i2=getIntent();
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.detail_array, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner1.setAdapter(adapter);
+        List<String> categories = new ArrayList<String>();
+        categories.add("Name");
+        categories.add("Gender");
+        categories.add("Address");
+        categories.add("Hospital");
+        categories.add("Date of Birth");
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, categories);
+
+        // Drop down layout style - list view with radio button
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // attaching data adapter to spinner
+        spinner1.setAdapter(dataAdapter);
 
 
 
-     //   String username=i2.getExtras().getString("emailid");
-
-
-
-
-
-
+        //   String username=i2.getExtras().getString("emailid");
 
 
         btnLogOut = (Button) findViewById(R.id.btnLogOut);
@@ -85,4 +85,3 @@ Spinner spinner1;
 
     }
 }
-
