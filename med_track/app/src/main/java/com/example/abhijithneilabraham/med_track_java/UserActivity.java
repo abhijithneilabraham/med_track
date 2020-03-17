74fec2f3b24626603199b0e598f87799bb78eabe
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2020-03-18 00:28:06 +0530

    huh. better now

--- med_track/app/src/main/java/com/example/abhijithneilabraham/med_track_java/UserActivity.java
+++ med_track/app/src/main/java/com/example/abhijithneilabraham/med_track_java/UserActivity.java
@@ -2,6 +2,7 @@ package com.example.abhijithneilabraham.med_track_java;
 
 import android.app.DatePickerDialog;
 import android.app.Dialog;
+import android.os.Build;
 import android.os.Bundle;
 
 import android.view.View;
@@ -24,27 +25,56 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
 public class UserActivity extends AppCompatActivity  {
-    Button btnLogOut,submit1,dob;
+    //initialisations
+
+    Button btnLogOut,submit1,dobclick;
     FirebaseAuth firebaseAuth;
-
     private DatePicker dpResult;
-
     private FirebaseAuth.AuthStateListener authStateListener;
     String age;
     FirebaseDatabase database = FirebaseDatabase.getInstance();
     public String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
     Spinner spinner1;
     EditText details;
-   public String headval,dataval;
+    public String headval,dataval,dob;
+    Calendar calendar=Calendar.getInstance();
+    int y=calendar.get(Calendar.YEAR)-18;
+    int m=calendar.get(Calendar.MONTH);
+    int d=calendar.get(Calendar.DATE);
+
+
+    //methods
     public void writeNewUser(String pid,String head,String data) {
         database.getReference(uid).child(pid).child(head).setValue(data);
     }
+    private String getAge(int year, int month, int day){
+        Calendar dob = Calendar.getInstance();
+        Calendar today = Calendar.getInstance();
 
+        dob.set(year, month, day);
+
+        int age = today.get(Calendar.YEAR) - dob.get(Calendar.YEAR);
+
+        if (today.get(Calendar.DAY_OF_YEAR) < dob.get(Calendar.DAY_OF_YEAR)){
+            age--;
+        }
+
+        Integer ageInt = new Integer(age);
+        String ageS = ageInt.toString();
+
+        return ageS;
+    }
+
+
+    //oncreate
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -53,14 +83,29 @@ public class UserActivity extends AppCompatActivity  {
         spinner1=(Spinner)findViewById(R.id.spinner1);
         details=(EditText)findViewById(R.id.usr);
         submit1=(Button)findViewById(R.id.submit);
-        dpResult = (DatePicker) findViewById(R.id.dpResult);
-        String dob=dpResult.getDayOfMonth()+"/"+dpResult.getMonth()+"/"+dpResult.getYear();
+        //dpResult = (DatePicker) findViewById(R.id.dpResult);
+        dobclick=(Button)findViewById(R.id.dob);
+
+        dobclick.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.N)
+            @Override
+            public void onClick(View v) {
+                DatePickerDialog datePickerDialog=new DatePickerDialog(UserActivity.this, new DatePickerDialog.OnDateSetListener() {
+                        @Override
+                        public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                             dob=dayOfMonth+"/"+month+"/"+year;
+                        }
+                    },y,m,d);
+                datePickerDialog.show();
+            }
+        });
+       // String dob=dpResult.getDayOfMonth()+"/"+dpResult.getMonth()+"/"+dpResult.getYear();
+
 
         List<String> categories = new ArrayList<String>();
         categories.add("Name");
         categories.add("Address");
         categories.add("Hospital");
-        categories.add("Date of Birth");
         Intent inu=getIntent();
         String idnum=inu.getExtras().getString("idnumber");
 
