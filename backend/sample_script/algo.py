76bb139659651aee8c6546aa4f9062641546b57b
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2020-03-24 20:22:01 +0530

    finished a simple algo for the calculation

--- backend/sample_script/algo.py
+++ backend/sample_script/algo.py
@@ -5,29 +5,44 @@ Created on Tue Mar 24 15:53:01 2020
 
 @author: abhijithneilabraham
 """
-from datetime import date,timedelta
-class tracker:
-    def __init__(self,d,dr,st):
+from datetime import date,timedelta,datetime
+class Tracker:
+    def __init__(self,d,dr,st,starttime):
         self.d=d
         self.dr=dr
         self.st=st
+        self.starttime=starttime
+        self.flag=0
+        self.maxdur=30
     def exptime(self,dosage,dur,stock):
         expiry=0
         if dur=="Day":
             expiry=stock//dosage
         elif dur=="Week":
-            expiry=stock//(dosage*7)
+            expiry=stock*7//(dosage)
         elif dur=="Month":
-            expiry=stock//(dosage*30)
+            expiry=stock*30//(dosage)
         return expiry
     
  
     #String date = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(new Date()); this is for java
     def updatetime(self,starttime):
-        d=self.d
-        dr=self.dr
-        st=self.st
-        deadline=starttime+timedelta(self.exptime(d,dr,st))
+        starttime = datetime.strptime(starttime, '%Y-%m-%d').date()
+
+        deadline=starttime+timedelta(self.exptime(self.d,self.dr,self.st))
+        
         return deadline
-track=tracker(20,30,200)
-print(track.updatetime(date.today()))
+    def calctime(self):
+        now=date.today()
+        balance=(self.updatetime(self.starttime)-now).days
+        if balance<self.maxdur:
+            self.flag=1
+            
+
+        return balance,self.flag
+    
+        
+        
+track=Tracker(2,"Day",200,"2020-01-01")
+remtime,flag=track.calctime()
+print("Remaining days",remtime,"with flag",flag)
