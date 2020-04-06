20d42cd55609a2ebeea1dd34d91fd8d3286e745b
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2020-04-07 04:25:07 +0530

    updated the UI of homefragment with an alertdialog

--- med_track/app/src/main/java/com/example/abhijithneilabraham/med_track_java/ui/home/HomeFragment.java
+++ med_track/app/src/main/java/com/example/abhijithneilabraham/med_track_java/ui/home/HomeFragment.java
@@ -1,10 +1,13 @@
 package com.example.abhijithneilabraham.med_track_java.ui.home;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -27,7 +30,7 @@ import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
 public class HomeFragment extends Fragment {
-    private Button buttonScan,btnlogout2,enterid,submit2;
+    private Button btnlogout2,enterid,submit2;
     private TextView textViewName, textViewAddress;
     EditText idedit;
 
@@ -42,7 +45,7 @@ public class HomeFragment extends Fragment {
         View root = inflater.inflate(R.layout.fragment_home, container, false);
 
          enterid=(Button) root.findViewById(R.id.enteridfr);
-        buttonScan = (Button)root.findViewById(R.id.qrfr);
+
         btnlogout2=(Button)root.findViewById(R.id.btnLogOut2fr);
 
 
@@ -52,34 +55,71 @@ public class HomeFragment extends Fragment {
         enterid.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                AlertDialog.Builder builderSingle = new AlertDialog.Builder(getContext());
+                builderSingle.setIcon(R.drawable.ic_launcher_background);
+                builderSingle.setTitle("Select One Name:-");
+
+                final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getContext(), android.R.layout.select_dialog_singlechoice);
+                arrayAdapter.add("Enter ID Manually");
+                arrayAdapter.add("Scan ID QR code");
+                String x="Enter ID Manually";
 
 
-                if(idedit.getVisibility()==v.GONE){
+                builderSingle.setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+
+                builderSingle.setAdapter(arrayAdapter, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String strName = arrayAdapter.getItem(which);
+                        AlertDialog.Builder builderInner = new AlertDialog.Builder(getContext());
+                        if (strName.equals(x)){
+                        builderInner.setMessage(strName+" In the text field below");
+                        }
+                        else{
+                            builderInner.setMessage("Scan the QR code containing the patient ID tag");
+
+                        }
+
+                        builderInner.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog,int which) {
+                                if(strName.equals("Enter ID Manually"))
+                                {
+                                    if(idedit.getVisibility()==v.GONE){
 
 
-                    idedit.setVisibility(v.VISIBLE);
+                                        idedit.setVisibility(v.VISIBLE);
+                                    }
+
+                                }
+                                else{
+                                    qrScan =  IntentIntegrator.forSupportFragment(HomeFragment.this);
+                                    qrScan.initiateScan();
+                                }
+                                dialog.dismiss();
+                            }
+                        });
+                        builderInner.show();
+                    }
+                });
+                builderSingle.show();
 
 
 
-
-
-                }else{
-                    idedit.setVisibility(v.GONE);
-                }
             }
         });
 
 
         //intializing scan object
-        qrScan =  IntentIntegrator.forSupportFragment(HomeFragment.this);
+
 
         //attaching onclick listener
-        buttonScan.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                qrScan.initiateScan();
-            }
-        });
+
         submit2=(Button)root.findViewById(R.id.submit2fr);
         submit2.setOnClickListener(new View.OnClickListener() {
                                        @Override
